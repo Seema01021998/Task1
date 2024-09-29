40de6a7cd498512c051807e191de0262a02328b4
Seema01021998 <seemasondhiya5@gmail.com>
2024-09-29 21:44:51 +0530

    second commit

---
+++ query2.sql
@@ -0,0 +1,11 @@
+-- SELECT * FROM public.employee_details 
+  insert into employee_details ( "FirstName"  ,"LastName" ,
+ "Email" ,"PhoneNumber"  , "HireDate ","Salary" , "DepartmentID" ,"IsActive"  ,"JobTitle ") 
+ values ('radhe','sondhiya','radhesondhiya5@gmail.com',9545333440,'2024-07-05',35000,1, 'true',
+ 'HR'),
+('Priya','sondhiya','priyasondhiya5@gmail.com',9878676545,'2023-06-05',45000,1, 'true',
+ 'finance'),
+('rahul','Iyer','rahutiyer5@gmail.com',8767654567,'2024-06-05',35000,1, 'true',
+ 'finance'), 
+ ('pooja','sondhiya','pojasondhiya5@gmail.com',7656879878,'2021-03-05',55000,1, 'true',
+ 'Marketing');
